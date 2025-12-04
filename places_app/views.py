41af5a4e804dc9ba7997d65ee1cd67db06a90b6d
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-04 12:51:53 +0500

    Add Image model and dynamic geojson rendering for places

--- places_app/views.py
+++ places_app/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
+from .models import Place
+
 
 def index(request):
-    return render(request, 'index.html')
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for place in Place.objects.all():
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    place.lng,
+                    place.lat
+                ]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "TODO",
+            }
+        }
+        places_geojson["features"].append(feature)
+
+    context = {
+        "places_geojson": places_geojson
+    }
+    return render(request, 'index.html', context)
