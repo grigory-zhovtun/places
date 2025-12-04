862d1b57c5383912b4df87467a2a1c1e5b011eb9
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-04 09:25:33 +0500

    Add model for images

--- places_app/admin.py
+++ places_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
 
-admin.site.register(Place)
+class ImageInline(admin.TabularInline):
+    model = Image
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+
+admin.site.register(Image)
--- places_app/models.py
+++ places_app/models.py
@@ -10,3 +10,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='imgs', verbose_name='Место')
+    image = models.ImageField('Картинка')
+    position = models.PositiveIntegerField('Позиция', default=0)
+
+    class Meta:
+        ordering = ['position']
+
+    def __str__(self):
+        return f'{self.position} {self.place.title}'
