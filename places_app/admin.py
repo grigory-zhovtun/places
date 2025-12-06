ba75cce4dd8fbe147048d47b06ea27d169094791
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-06 21:14:35 +0500

    Add image preview in admin for better visualization of uploaded images

--- places_app/admin.py
+++ places_app/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.html import format_html
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;">',
+                obj.image.url
+            )
+        return '-'
+
+    get_preview.short_description = 'Preview'
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
