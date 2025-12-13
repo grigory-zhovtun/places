8b26b428b97934bd42633358077745add719b6a4
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-13 16:09:20 +0500

    Integrate TinyMCE for rich text editing; update `description_long` field in `Place` model and configure URLs/settings.

--- places/urls.py
+++ places/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('places/', include('places_app.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places_app/models.py
+++ places_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField("Название", max_length=200)
     description_short = models.TextField("Краткое описание", blank=True)
-    description_long = models.TextField("Полное описание", blank=True)
+    description_long = HTMLField('Полное описание')
     lng = models.FloatField("Долгота")
     lat = models.FloatField("Широта")
 
