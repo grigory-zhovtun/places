e00d2b1f199337708ed517119b4346b9cfdbd436
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-16 22:45:59 +0500

    Make `title` field in `Place` model unique to enforce data consistency, update model definition.

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Название', max_length=200)
+    title = models.CharField('Название', max_length=200, unique=True)
     short_description = models.TextField('Краткое описание', blank=True)
     long_description = HTMLField('Полное описание', blank=True)
     lng = models.FloatField('Долгота')
