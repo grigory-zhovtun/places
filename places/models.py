de17461ee2eeef6dba4f7f9a142ff4cd15faa43b
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-16 21:53:12 +0500

    Allow blank values for `description_long` field in `Place` model and update migration.

--- places/models.py
+++ places/models.py
@@ -5,7 +5,7 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     title = models.CharField("Название", max_length=200)
     description_short = models.TextField("Краткое описание", blank=True)
-    description_long = HTMLField('Полное описание')
+    description_long = HTMLField('Полное описание', blank=True)
     lng = models.FloatField("Долгота")
     lat = models.FloatField("Широта")
 
